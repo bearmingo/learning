35f7165708180c7a6b16aa9a4f1fa9d7797c5ae8
bearmingo <bearmingo@gmail.com>
2019-10-05 22:36:20 +0800

    add solution of 57 insert interval

--- algorithm/leetcode/00057-insert-interval/py2/main.py
+++ algorithm/leetcode/00057-insert-interval/py2/main.py
@@ -10,7 +10,7 @@ class Solution:
         index = 0
         # add all intervals ending before newInterval starts
         while index < l and intervals[index][1] < newInterval[0]:
-            pass
+            index += 1
         ret.extend(intervals[:index])
         
         # merge all overlapping intervals to one considering newInterval
@@ -30,4 +30,5 @@ class Solution:
 if __name__ == "__main__":
     s = Solution()
     print(s.insert([[1,3],[6,9]], [2,5]))
+    print(s.insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))
     print(s.insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))
